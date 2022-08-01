0806215df2832fc0004f6d05db6d1d2242af72cc
smarthug <kirklayer@gmail.com>
2022-08-01 14:40:53 +0900

    second place implemented

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
 import { GetDrawNumber } from '../utils/time'
-import { Button } from '@mui/material'
+import { Button, ButtonGroup } from '@mui/material'
 import _ from 'lodash'
-
+import { numToKorean } from 'num-to-korean';
 //https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=990
 
 
@@ -36,13 +36,15 @@ function SelectRandomNumber(length) {
 
 export default function Home() {
     const [drawNum, setDrawNum] = useState();
-    const [winNum, setWinNum] = useState([]);
+    const [winNum, setWinNum] = useState([0]);
+    const [bonusNum, setBonusNum] = useState(0)
 
-    const [prize1, setPrize1] = useState();
-    const [prize3, setPrize3] = useState();
-    const [prize4, setPrize4] = useState();
-    const [prize5, setPrize5] = useState();
-    const [money, setMoney] = useState();
+    const [prize1, setPrize1] = useState(0);
+    const [prize2, setPrize2] = useState(0);
+    const [prize3, setPrize3] = useState(0);
+    const [prize4, setPrize4] = useState(0);
+    const [prize5, setPrize5] = useState(0);
+    const [money, setMoney] = useState(0);
 
     useEffect(() => {
         window.exp = {}
@@ -73,11 +75,13 @@ export default function Home() {
                     // local storage 에 저장 ...
                     localStorage.setItem('data', JSON.stringify(data))
                     localStorage.setItem('drawNum', data.drwNo)
-                    localStorage.setItem('winNum', JSON.stringify([data.drwtNo1, data.drwtNo2, data.drwtNo3, data.drwtNo4, data.drwtNo5, data.drwtNo6, data.bnusNo]))
+                    localStorage.setItem('winNum', JSON.stringify([data.drwtNo1, data.drwtNo2, data.drwtNo3, data.drwtNo4, data.drwtNo5, data.drwtNo6]))
+                    localStorage.setItem('bonusNum', JSON.stringify(data.bnusNo))
 
                     // setState , 회차 , 숫자 
                     setDrawNum(localStorage.getItem('drawNum'))
                     setWinNum(localStorage.getItem('winNum'))
+                    setBonusNum(localStorage.getItem('bonusNum'))
                 })
                 .catch(function (error) {
                     // handle error
@@ -91,6 +95,7 @@ export default function Home() {
             // 여기서 get Item 한걸로 setstate ...
             setDrawNum(localStorage.getItem('drawNum'))
             setWinNum(JSON.parse(localStorage.getItem('winNum')))
+            setBonusNum(localStorage.getItem('bonusNum'))
             console.log(JSON.parse(localStorage.getItem('winNum')));
             // console.log();
         }
@@ -98,20 +103,37 @@ export default function Home() {
 
 
     function checkLotto(inputMoney) {
-        const winNums = [8, 9, 20, 25, 29, 33]
+        // const winNums = [8, 9, 20, 25, 29, 33]
+        // const bonusNum = 7
 
+        const winNums = winNum
+        // const bonusNum = bonusNum
         // 배열로 만들고 , 배열의 숫자를 올리자,
-        // 5등 , 4등 , 3등 , 1등 
-        const prizeCount = [0, 0, 0, 0]
+        // 5등 , 4등 , 3등 , 1등 ,2 등
+        const prizeCount = [0, 0, 0, 0, 0]
+
+        console.log(winNums);
+        console.log(bonusNum)
 
         for (let index = 0; index < inputMoney; index++) {
             const pickedNums = AutoPick();
 
             const result = _.intersection(winNums, pickedNums)
-            if (result.length > 2) {
+
+            if (result.length === 5 && pickedNums.includes(bonusNum)) {
+                // if (result.length === 5 ) {
+                // 2등 
+                prizeCount[4] += 1;
+                console.log(pickedNums);
+                if (pickedNums.includes(bonusNum)) {
+                    console.log("2등 당첨");
+                }
+            } else if (result.length > 2) {
 
                 // console.log(result)
                 prizeCount[result.length - 3] += 1;
+
+
             }
         }
         console.log(prizeCount)
@@ -121,8 +143,11 @@ export default function Home() {
         setPrize3(prizeCount[2])
         setPrize1(prizeCount[3])
 
-        const totalMoney = prizeCount[0] * 5000 + prizeCount[1] * 50000 + prizeCount[2] * 1500000 + prizeCount[3] * 6118853344
-        setMoney(totalMoney)
+        setPrize2(prizeCount[4]);
+
+        const totalMoney = prizeCount[0] * 5000 + prizeCount[1] * 50000 + prizeCount[2] * 1500000 + prizeCount[3] * 6118853344 + prizeCount[4] * 53260477
+
+        setMoney(numToKorean(totalMoney, 'mixed'))
     }
 
     return (
@@ -130,26 +155,30 @@ export default function Home() {
             <h1>{drawNum} 회</h1>
             <div>
                 {
-                    winNum && winNum.map((v, i) => {
+                    winNum.length > 1 && winNum.map((v, i) => {
                         return (
                             <span style={{ margin: "8px" }} key={i}>{v}</span>
                         )
                     })
                 }
-            </div>
-            <div>
-                <Button variant="contained" color="success" onClick={()=>checkLotto(1000)} >100만원 자동구매</Button>
-                <Button variant="contained" color="success" onClick={()=>checkLotto(10000)} >1000만원 자동구매</Button>
-                <Button variant="contained" color="success" onClick={()=>checkLotto(100000)} >1억원 자동구매</Button>
+                <span style={{ margin: "8px" }} >+{bonusNum}</span>
             </div>
             <div>
                 <div>1등 {prize1}회</div>
+                <div>2등 {prize2}회</div>
                 <div>3등 {prize3}회</div>
                 <div>4등 {prize4}회</div>
                 <div>5등 {prize5}회</div>
             </div>
             <div>수령금: {money}원</div>
-        </div>
+            <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                <Button variant="contained" color="success" onClick={() => checkLotto(1000)} >100만원 자동구매</Button>
+                <Button variant="contained" color="success" onClick={() => checkLotto(10000)} >1000만원 자동구매</Button>
+                <Button variant="contained" color="success" onClick={() => checkLotto(100000)} >1억원 자동구매</Button>
+                {/* <Button variant="contained" color="success" onClick={() => checkLotto(1000000)} >10억원 자동구매</Button>
+                <Button variant="contained" color="success" onClick={() => checkLotto(10000000)} >100억원 자동구매</Button> */}
+            </ButtonGroup>
+        </div >
     )
 }
 
